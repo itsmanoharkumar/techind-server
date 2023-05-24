bc6c68afabe7ab1d651c7da93e5a9080fc3ef24a
Manohar <itsmanoharkumar@gmail.com>
2023-05-25 03:16:32 +0530

    Saint Pipeline, please give me the green light

---
+++ config/plugins.js
@@ -0,0 +1,26 @@
+module.exports = ({env}) => ({
+  "vercel-deploy": {
+    enabled: true,
+    config: {
+      deployHook: env("VERCEL_DEPLOY_HOOK"),
+      apiToken: env("VERCEL_API_TOKEN"),
+      appFilter: env("VERCEL_APP_FILTER"),
+      teamFilter: env('VERCEL_TEAM_FILTER'),
+      roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
+    }
+  },
+  upload: {
+    config: {
+      provider: "strapi-provider-upload-azure-storage",
+      providerOptions: {
+        account: env("STORAGE_ACCOUNT"),
+        accountKey: env("STORAGE_ACCOUNT_KEY"),//either account key or sas token is enough to make authentication
+        sasToken: env("STORAGE_ACCOUNT_SAS_TOKEN"),
+        serviceBaseURL: env("STORAGE_URL"), // optional
+        containerName: env("STORAGE_CONTAINER_NAME"),
+        defaultPath: "assets",
+        cdnBaseURL: env("STORAGE_CDN_URL"), // optional
+      },
+    },
+  },
+});
